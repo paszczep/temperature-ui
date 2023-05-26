420601ed8315061e44589a96ca29f6b1b8fe10cf
pawel <szczepanski.pawel.2@gmail.com>
2023-05-27 00:46:40 +0200

    save

--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 from pathlib import Path
 from dotenv import load_dotenv
 from os import getenv
@@ -22,6 +23,17 @@ def create_app():
 
     db.init_app(app)
     migrate.init_app(app, db)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    from .models import User
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        # since the user_id is just the primary key of our user table, use it in the query for the user
+        return User.query.get(int(user_id))
 
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
--- application/models.py
+++ application/models.py
@@ -1,7 +1,8 @@
 from . import db
+from flask_login import UserMixin
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
@@ -12,3 +13,15 @@ class Container(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30))
     status = db.Column(db.String(30))
+
+
+class Thermometer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(1000))
+    reading = db.Column(db.String(1000))
+    reading_time = db.Column(db.String(1000))
+    capture_time = db.Column(db.String(1000))
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
