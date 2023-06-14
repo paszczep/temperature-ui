eef7b8187436fefe7c040c55c985d020a293355b
pawel <szczepanski.pawel.2@gmail.com>
2023-06-15 01:00:51 +0200

    ouch

--- application/input.py
+++ application/input.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request
 from flask_wtf import FlaskForm
-from wtforms.fields import DateField, TimeField, SelectMultipleField, StringField
+from wtforms.fields import DateField, TimeField, SelectMultipleField, StringField, SubmitField
 from wtforms.validators import InputRequired, Length
 from wtforms_alchemy import QuerySelectMultipleField, WeekDaysField
 from wtforms import widgets
@@ -20,15 +20,19 @@ def tasks():
     )
 
 
-class NameChangeForm(FlaskForm):
+class ThermometerSelect(QuerySelectMultipleField):
+    widget = widgets.ListWidget(prefix_label=False)
+    option_widget = widgets.CheckboxInput()
+
+
+class TaskForm(FlaskForm):
     given_name = StringField('given_name')
-    thermometer_select = SelectMultipleField('thermometer_select')
+    measure_selection = ThermometerSelect('choices')
 
 
 def render_task_template(
         all_containers: list[Container],
         task_container: Container,
-        thermometers: list[Thermometer],
         form: FlaskForm
     ):
     return render_template(
@@ -36,29 +40,31 @@ def render_task_template(
         form=form,
         task_container=task_container,
         containers=all_containers,
-        thermometers=thermometers,
     )
 
 
 @input.route("/task/<string:container>", methods=["POST", "GET"])
 def new_task(container: str):
     all_containers = Container.query.all()
-
     the_container = [cont for cont in all_containers if cont.name == container].pop()
-    all_thermometers = Thermometer.query.all()
-    form = NameChangeForm()
-    new_name = request.form.get('given_name')
-    measures = request.form.get('thermometer_select')
+
+    form = TaskForm(data={"choices": the_container.measures})
+    form.measure_selection.choices = Thermometer.query.all()
+
+    new_name = form.given_name.data
+    new_selection = form.measure_selection.data
+    print(new_name)
     if form.validate_on_submit():
+
         if new_name:
             the_container.given_name = new_name
-            db.session.commit()
-        print(measures)
+        the_container.measures.clear()
+        the_container.measures.extend(new_selection)
+        db.session.commit()
 
     return render_task_template(
         form=form,
         all_containers=all_containers,
         task_container=the_container,
-        thermometers=all_thermometers
     )
 
